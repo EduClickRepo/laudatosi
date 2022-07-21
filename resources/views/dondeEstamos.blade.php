44ba763a545ffd3592608d04e767916be4dd690e
carolnata <109317389+carolnata@users.noreply.github.com>
2022-07-21 17:48:35 -0500

    Update dondeEstamos.blade.php

--- resources/views/dondeEstamos.blade.php
+++ resources/views/dondeEstamos.blade.php
@@ -27,7 +27,7 @@
         
 		<h1 style="color:#64DD17;text-align: center;font-family: sans-serif">¿Dónde Estamos?</h1>
         <h3 style="color:black;text-align: center;font-family: sans-serif">Estamos ubicados en la Calle 9A# 4-03 E. Esta es nuestra unica sede</h3>
-<p style= "text-align:center"><img src =/imagenes/foto fundación.jpeg">
+<p style= "text-align:center"><img src ="/imagenes/foto fundación.jpeg">
 
 		<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
